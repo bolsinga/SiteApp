f581d34fd8d25dec1d212c9223f90a17e2b7577c
Greg Bolsinga <bolsinga@mac.com>
2023-03-24 18:15:56 -0700

    Initial Commit

---
+++ SiteApp/SiteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiteAppApp.swift
+//  SiteApp
+//
+//  Created by Greg Bolsinga on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SiteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

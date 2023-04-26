441cae2e2ab6b089225361d62920bcc436e0f82c
Greg Bolsinga <bolsinga@mac.com>
2023-04-26 09:53:06 -0700

    Site Archive

--- SiteApp/SiteAppApp.swift
+++ SiteApp/SiteAppApp.swift
@@ -5,13 +5,35 @@
 //  Created by Greg Bolsinga on 3/24/23.
 //
 
+import Site
 import SwiftUI
 
 @main
 struct SiteAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+  @State var vault: Vault? = nil
+  @State var error: Error? = nil
+
+  var body: some Scene {
+    WindowGroup {
+      Group {
+        if let vault {
+          ArchiveCategoryList(vault: vault)
+        } else if let error {
+          Text(error.localizedDescription)
+        } else {
+          ProgressView()
         }
+      }.task {
+        guard let url = URL(string: "https://www.bolsinga.com/json/shows.json") else {
+          fatalError("need this url")
+        }
+
+        do {
+          vault = try await Vault.load(url: url)
+        } catch {
+          self.error = error
+        }
+      }
     }
+  }
 }

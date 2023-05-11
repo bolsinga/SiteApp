afa07c6340206dd660dbab3684f67b03f100ba90
Greg Bolsinga <bolsinga@mac.com>
2023-05-11 14:41:29 -0700

    Use VaultView (#4)

--- SiteApp/SiteAppApp.swift
+++ SiteApp/SiteAppApp.swift
@@ -10,30 +10,9 @@ import SwiftUI
 
 @main
 struct SiteAppApp: App {
-  @State var vault: Vault? = nil
-  @State var error: Error? = nil
-
   var body: some Scene {
     WindowGroup {
-      Group {
-        if let vault {
-          ArchiveCategoryList(vault: vault)
-        } else if let error {
-          Text(error.localizedDescription)
-        } else {
-          ProgressView()
-        }
-      }.task {
-        guard let url = URL(string: "https://www.bolsinga.com/json/shows.json") else {
-          fatalError("need this url")
-        }
-
-        do {
-          vault = try await Vault.load(url: url)
-        } catch {
-          self.error = error
-        }
-      }
+      VaultView(url: URL(string: "https://www.bolsinga.com/json/shows.json")!)
     }
   }
 }

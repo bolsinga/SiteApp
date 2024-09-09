2a96aeac7cba2fecc78e54b50cabc6a3eabf662e
Greg Bolsinga <bolsinga@mac.com>
2024-09-09 12:49:21 -0700

    Add Quick Action support via SceneDelegate
    - See branch in site framework for notes.
    - These changes in teh app will show the short cut UI on iOS, but just know know how to wire those
    strings up to code elsewhere.
    - See #808

--- SiteApp/SiteAppApp.swift
+++ SiteApp/SiteAppApp.swift
@@ -8,13 +8,39 @@
 import Site
 import SwiftUI
 
+struct Inner : View {
+//  @State private var archiveNavigation = ArchiveNavigation()
+  @SceneStorage("navigation.state") private var archiveNavigation = ArchiveNavigation()
+
+  var body: some View {
+    EmptyView()
+  }
+}
+
 @main
 struct SiteAppApp: App {
+  @Environment(\.scenePhase) private var scenePhase
+
+#if !os(macOS)
+  @UIApplicationDelegateAdaptor(ArchiveAppDelegate.self) private var appDelegate
+#endif
+
   @State private var model = SiteModel(urlString: "https://www.bolsinga.com/json/shows.json")
+
+  @SceneStorage("navigation.state") private var archiveNavigation = ArchiveNavigation()
 
   var body: some Scene {
     WindowGroup {
       SiteView(model)
+        .onAppear {
+          appDelegate.thing = 3
+          appDelegate.archiveNavigation = archiveNavigation
+        }
+    }
+    .onChange(of: scenePhase) { _, newScenePhase in
+      if newScenePhase == .active {
+        appDelegate.thing = 3
+      }
     }
     #if !os(tvOS)
       .commands {
